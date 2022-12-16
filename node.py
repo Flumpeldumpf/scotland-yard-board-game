891c1197d54a010021487cfc231a9c0c4c9e56d8
Alex Andersen <alexstephenandersen@gmail.com>
2022-12-15 19:27:18 -0800

    First changes from Alex

--- node.py
+++ node.py
@@ -1,1 +1,8 @@
+class node:
+    def __init__(self, number, taxi, bus, metro):
+        self.number = number
+        self.taxi = taxi
+        self.bus = bus
+        self.metro = metro
 
+    def create_node():
