69db441fe6ad55720101d8a21093136a2733dbfe
Alex Andersen <alexstephenandersen@gmail.com>
2022-12-15 21:27:12 -0800

    file reader

--- map.py
+++ map.py
@@ -1,2 +1,13 @@
 class map:
-    def __init__(self, node_list, )
+    def __init__(self, node_list, ):
+        self.node_list = node_list
+
+    def makemap():
+        f = open("connections.txt", "r")
+        
+        line = f.readline()
+        while line:
+            list = line.split(" ")
+            node(list[0], list)
+
+        f.close()
