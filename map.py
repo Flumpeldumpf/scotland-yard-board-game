c4b00c22d75809eee8f93f5d178fac288ebe63e2
tamsenm <46328791+tamsenm@users.noreply.github.com>
2022-12-16 16:05:19 -0800

    add networkx to map

--- map.py
+++ map.py
@@ -1,10 +1,18 @@
 from node import node
+import networkx as nx
 
 class map:
     def __init__(self, node_list):
         self.node_list = node_list
+        self.taxis = nx.Graph()
+        self.buses = nx.Graph()
+        self.metros = nx.Graph()
 
     def makemap(self):
+        self.taxis.add_nodes_from(range(1,200))
+        self.buses.add_nodes_from(range(1,200))
+        self.metros.add_nodes_from(range(1,200))
+
         f = open("scotland_yard.txt", "r")
         
         line = f.readline()
@@ -14,6 +22,17 @@ class map:
             list[3] = list[3].split("\n")[0]
             n = node(list[0], list[1], list[2], list[3])
             self.node_list.append(n)
+
+            for i in list[1].split(","):
+                if (i != 0):
+                    self.taxis.add_edge(int(list[0]), int(i))
+            for i in list[2].split(","):
+                if (i != 0):
+                    self.buses.add_edge(int(list[0]), int(i))
+            for i in list[3].split(","):
+                if (i != 0):
+                    self.metros.add_edge(int(list[0]), int(i))
+
             line = f.readline()
 
         f.close()
