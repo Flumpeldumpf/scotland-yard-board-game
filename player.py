0c507f7ab5768534b4a3a9eb403c2b434468da60
tamsenm <46328791+tamsenm@users.noreply.github.com>
2022-12-17 23:04:34 -0800

    blah

--- player.py
+++ player.py
@@ -10,24 +10,7 @@ class player:
         print(self.resources[0], self.resources[1], self.resources[2])
 
     def move(self, pos, transport):
-        index = 0
-        if transport == 2:
-            index = 0
-        elif transport == 3:
-            index = 1
-        elif transport == 5:
-            index = 2
-        if pos.occupied == 0:
-            if self.resources[index] > 0:
-                if self.pos.check_connectivity(pos) % transport == 0 and transport > 1:
-                    self.resources[index] = self.resources[index] - 1
-                    self.pos.occupied = 0;
-                    self.pos = pos
-                    self.pos.occupied = 1;
-                    return("Move successful!")
-                else:
-                    return("Your method of transportation does not connect to this node.")
-            else:
-                return("You are out of tokens for this type of transport.")
-        else:
-            return("Node is already occupied");
+        self.resources[index] = self.resources[index] - 1
+        self.pos.occupied = 0;
+        self.pos = pos
+        self.pos.occupied = 1;
