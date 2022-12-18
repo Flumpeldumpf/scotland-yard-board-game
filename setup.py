17720cbb49714a87ddf1bab6a4c6776045d6001b
tamsenm <46328791+tamsenm@users.noreply.github.com>
2022-12-17 20:48:00 -0800

    update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ class setup:
     def __init__(self):
         pass
     
+    def take_turn(self, node):
+        #print out resources and possible moves
+        print("Input the point you would like to move to.")
+        target = input()
+        print("Input your method of transportation.")
+        transport = input()
     def generate_start_positions(self):
         possible_sp = [13, 26, 29, 34, 50, 53, 91, 94,
          103, 112, 117, 132, 138, 141, 155, 174, 197, 198]
